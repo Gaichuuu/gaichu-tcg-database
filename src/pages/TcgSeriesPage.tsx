2ea8684dd6e889f2b2d657d725eaf7fe0ae200f7
nnsaori <saori07208@gmail.com>
2025-03-25 22:31:55 -0700

    show firebase data on the series view

--- src/pages/TcgSeriesPage.tsx
+++ src/pages/TcgSeriesPage.tsx
@@ -1,54 +1,13 @@
 // src/pages/TcgSeriesPage.tsx
 import React from "react";
-import { useNavigate } from "react-router-dom";
-import Tile from "../components/Tile";
-
-interface SeriesData {
-  id: string;
-  name: string;
-  logo: string;
-  description: string;
-}
-
-const seriesData: SeriesData[] = [
-  {
-    id: "wm",
-    name: "WrennyMoo",
-    logo: "/src/assets/wmLogo.png",
-    description: "2 releases",
-  },
-  {
-    id: "brainwash",
-    name: "Brainwash TCG",
-    logo: "/src/assets/tempLogo.png",
-    description: "1 release",
-  },
-  // add more series as needed
-];
+import CollectionList from '../components/CollectionList';
 
 const TcgSeriesPage: React.FC = () => {
-  const navigate = useNavigate();
 
   return (
     <div className="container mx-auto p-4">
-      <h2 className="mb-6 text-2xl font-bold">Series</h2>
-      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-        {seriesData.map((series) => (
-          <Tile key={series.id} onClick={() => navigate(`/cards/${series.id}`)}>
-            <div className="flex h-full flex-col items-center justify-center">
-              <img
-                src={series.logo}
-                alt={series.name}
-                className="mb-4 max-h-20 object-contain"
-              />
-              <h3 className="text-lg font-semibold">{series.name}</h3>
-              <p className="text-secondaryText text-center">
-                {series.description}
-              </p>
-            </div>
-          </Tile>
-        ))}
-      </div>
+    <h2 className="mb-6 text-2xl font-bold">Series</h2>
+    <CollectionList />
     </div>
   );
 };
