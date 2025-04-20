dcff5682aef4b3a42848eabba3cc0b221d8a17cb
CamelJR <aarcampion@gmail.com>
2025-04-19 22:46:59 -0700

    Build errors

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,17 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    chunkSizeWarningLimit: 1000,
+
+    rollupOptions: {
+      onwarn(warning, defaultHandler) {
+        if (warning.code === "MODULE_LEVEL_DIRECTIVE") return;
+
+        defaultHandler(warning);
+      },
+    },
+  },
 });
