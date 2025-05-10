0bcea08be032b470d20830dc8baf66d1efb82c47
nnsaori <saori07208@gmail.com>
2025-05-09 19:09:25 -0700

    switch loading Series data depending to the environment

--- src/hooks/getCollection.tsx
+++ src/hooks/getCollection.tsx
@@ -2,15 +2,32 @@ import { useQuery } from "@tanstack/react-query";
 import { fetchCardDetail, fetchCards } from "../services/CollectionCardService";
 import { fetchSeries } from "../services/CollectionSeriesService";
 import { fetchSets } from "../services/CollectionSetService";
+import { IS_USE_LOCAL_DATA } from "../services/Constants";
+import { getJsonSeries } from "../services/JsonCollectionSeriesService";
+import { Result } from "../services/Result";
 import { CollectionCard } from "../types/CollectionCard";
 import { SeriesAndSet, SetAndCard } from "../types/MergedCollection";
 
-export const getSeries = () => {
-  return useQuery<SeriesAndSet[]>({
+export const getSeries = (): Result<SeriesAndSet[], Error> => {
+  if (IS_USE_LOCAL_DATA) {
+    return {
+      data: getJsonSeries(),
+      error: undefined,
+      isLoading: false,
+    };
+  }
+
+  const queryResult = useQuery<SeriesAndSet[]>({
     queryKey: ["SeriesList"],
     queryFn: fetchSeries,
     staleTime: 1000 * 60 * 5,
   });
+
+  return {
+    data: queryResult.data,
+    error: queryResult.error || undefined,
+    isLoading: queryResult.isLoading,
+  };
 };
 
 export const getSets = (seriesShortName?: string) => {
