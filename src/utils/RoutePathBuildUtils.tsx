1f7382d7b1a80a10a4313e308e802ee6961f2423
nnsaori <saori07208@gmail.com>
2025-07-11 20:05:09 -0700

    show sortby in url, but not in Breadcrumbs

--- src/utils/RoutePathBuildUtils.tsx
+++ src/utils/RoutePathBuildUtils.tsx
@@ -9,6 +9,44 @@ export const SetListPath = SeriesListPath + "/:seriesShortName";
 export const CardListPath = SetListPath + "/sets/:setShortName";
 export const CardDetailPath = CardListPath + "/card/:sortByAndCardName";
 
+export enum PathSegments {
+  Cards = 0,
+  CardName = 1,
+  Sets = 2,
+  SetName = 3,
+  Card = 4,
+  SortByAndCardName = 5,
+  // Example: /cards/mz/sets/promo/card/99.3_Sunlight
+}
+
+export interface BreadcrumbItem {
+  label: string;
+  routeTo?: string | undefined;
+}
+
+export function getBreadcrumbItems(locationPathname: string): BreadcrumbItem[] {
+  const originalSegments = locationPathname.split("/").filter(Boolean);
+  return (
+    originalSegments
+      .map((segment, index) => {
+        if (index === PathSegments.SortByAndCardName) {
+          const { cardName } = parseSortAndNameRegex(
+            decodeURIComponent(segment),
+          );
+          // Handle for card name segment.
+          // label: Use the card name directly without sortBy number
+          // routeTo: No need to link to this segment
+          return { label: cardName, routeTo: undefined };
+        }
+        const label = decodeURIComponent(segment);
+        const routeTo = "/" + originalSegments.slice(0, index + 1).join("/");
+        return { label, routeTo: routeTo };
+      })
+      // Filter out unwanted segments "sets" and "card"
+      .filter((item) => item.label !== "sets" && item.label !== "card")
+  );
+}
+
 export function getSetListPath(seriesShortName: string): string {
   const path = generatePath(SetListPath, {
     seriesShortName: encodeURIComponent(seriesShortName),
