774d5a0d0bc6f03ec0167166ebc607328d48efd8
CamelJR <aarcampion@gmail.com>
2025-10-03 22:16:08 -0700

    Adds the score variable

--- src/types/news.tsx
+++ src/types/news.tsx
@@ -2,12 +2,12 @@ export type NewsPost = {
   id: string;
   slug: string;
   title: string;
-  subtitle?: string;
   author?: string;
   tags?: string[];
   hero_url?: string;
   excerpt: string;
   body_html?: string;
   created_at: number;
+  score?: number;
   searchPrefixes?: string[];
 };
