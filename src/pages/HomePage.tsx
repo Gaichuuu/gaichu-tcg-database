ae5b0d978a292c23e71741663c4f45ba6d018aa8
CamelJR <aarcampion@gmail.com>
2025-10-15 23:40:02 -0700

    Removes extra padding

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -9,15 +9,15 @@ const HomePage: React.FC = () => {
   const { data: latest } = useLatestNews(3);
 
   return (
-    <div className="flex flex-col items-center justify-center p-4">
+    <div className="mt-1 flex flex-col items-center justify-center p-0">
       <p className="mb-4 text-5xl tracking-wide">Welcome</p>
       <p className="mb-4 max-w-xl text-lg">
-        Your #2 source for parody and homemade card games.
+        Your #2 source for parody and bootleg card games.
       </p>
       <Link to="/cards" className="button">
         Browse Cards
       </Link>
-      <section className="mx-auto mt-8 max-w-6xl px-4">
+      <section className="mx-auto mt-8 max-w-6xl px-0">
         <div className="mb-2 flex items-baseline justify-between">
           <h3>Latest News</h3>
           {/* <Link to="/news" className="text-interactiveText">
