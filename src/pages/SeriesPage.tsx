20596c1fd756c97f1791198b3b9bfdaf61be625b
CamelJR <aarcampion@gmail.com>
2025-10-16 14:08:20 -0700

    Minor fix

--- src/pages/SeriesPage.tsx
+++ src/pages/SeriesPage.tsx
@@ -1,22 +1,26 @@
 // src/pages/SeriesPage.tsx
 import React from "react";
 import { useNavigate, useParams } from "react-router-dom";
-import SetsList from "@/components/ListComponent/SetListConponent";
+import SetsList from "@/components/ListComponent/SetListComponent";
 import { useSets } from "@/hooks/useCollection";
 import { getCardListPath } from "@/utils/RoutePathBuildUtils";
 
 const SeriesPage: React.FC = () => {
-  const { seriesShortName } = useParams();
-  const { data: collectionSet, error } = useSets(seriesShortName);
+  const { seriesShortName = "" } = useParams<"seriesShortName">();
+  const seriesKey = decodeURIComponent(seriesShortName);
+
+  const { data: collectionSet, error } = useSets(seriesKey);
   const navigate = useNavigate();
+
   if (error) return <p>Something went wrong...</p>;
+
   return (
     <div className="container mx-auto p-0">
-      <h3 className="mb-2">{seriesShortName} sets</h3>
+      <h3 className="mb-2">{seriesKey} sets</h3>
       <SetsList
         collectionSet={collectionSet ?? []}
         onClickSet={(set) => {
-          navigate(getCardListPath(seriesShortName ?? "", set));
+          navigate(getCardListPath(seriesKey, set));
         }}
       />
     </div>
