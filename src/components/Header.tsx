1fbcf4258d06934fd09c6430cdb98e69c663b572
CamelJR <aarcampion@gmail.com>
2025-10-15 23:39:09 -0700

    Decreases padding for footer/header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,8 +4,8 @@ import { Link } from "react-router-dom";
 
 const Header: React.FC = () => {
   return (
-    <header className="bg-navBg border-secondaryBorder border-b shadow-md">
-      <div className="mx-auto flex max-w-7xl items-center justify-between px-4 py-2">
+    <header className="bg-navBg border-secondaryBorder/40 border-b-2 shadow-xl">
+      <div className="mx-auto flex max-w-7xl items-center justify-between px-4 py-1">
         <Link to="/" className="flex items-center space-x-2">
           <img
             src="https://gaichu.b-cdn.net/assets/logo.gif"
