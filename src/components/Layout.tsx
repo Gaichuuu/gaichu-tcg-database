73d2c8583cdbdd764903075cf60dafe34d72ecd6
CamelJR <aarcampion@gmail.com>
2026-02-06 22:48:18 -0800

    Refactor to home screen layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -8,7 +8,7 @@ interface LayoutProps {
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <div className="bg-mainBg">
-      <div className="container mx-auto px-4 py-4">
+      <div className="container mx-auto px-4 py-2">
         <Breadcrumbs />
 
         <main>{children}</main>
