c4c15209be7f836e7528efb4d43496268c44c89f
CamelJR <aarcampion@gmail.com>
2026-02-08 13:22:23 -0800

    Moves canonical url from index to layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 import Breadcrumbs from "./Breadcrumbs";
 
 interface LayoutProps {
@@ -6,8 +7,12 @@ interface LayoutProps {
 }
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
+  const { pathname } = useLocation();
+  const canonicalUrl = `https://gaichu.com${pathname}`;
+
   return (
     <div className="bg-mainBg">
+      <link rel="canonical" href={canonicalUrl} />
       <div className="container mx-auto px-4 pt-2 pb-8">
         <Breadcrumbs />
 
