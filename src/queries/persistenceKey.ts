0b1530c2619f734971bcfb620972bf022f3e8af3
CamelJR <aarcampion@gmail.com>
2026-02-23 23:09:46 -0800

    Limits homepage featured sets

--- src/queries/persistenceKey.ts
+++ src/queries/persistenceKey.ts
@@ -1,3 +1,3 @@
-const PERSIST_VERSION = "v9"; // update to invalidate cache
+const PERSIST_VERSION = "v10"; // update to invalidate cache
 const PERSIST_KEY = `gaichu-query-cache-${PERSIST_VERSION}`;
 export { PERSIST_KEY };
